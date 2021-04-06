e3ca734cd83ed74e346d583d21c38ad60b9809fc
Simon C Blyth <simoncblyth@gmail.com>
2021-04-06 15:09:30 +0100

    images for https://simoncblyth.bitbucket.io/env/presentation/lz_opticks_optix7_20210406.html

---
+++ env/presentation/OptiXTest/parade_hyperboloid_grid.sh
@@ -0,0 +1,10 @@
+# /tmp/parade_hyperboloid_grid_env.sh
+tmin=0.1
+geometry=parade
+gridmodulo=3
+gridsingle=
+gridspec=-10:11:2,-10:11:2,0:6:3
+gridscale=200.0
+eye=-0.7,-0.7,0.15
+cameratype=0
+layers=20
