dfc0daf1232f095e21ef10f4d0f48dac34454654
Simon C Blyth <simoncblyth@gmail.com>
2023-04-28 08:08:35 +0100

    presentation ready

--- env/Documents/Geant4OpticksWorkflow7/copy_from_Documents.sh
+++ env/Documents/Geant4OpticksWorkflow7/copy_from_Documents.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -l 
+pfx=Geant4OpticksWorkflow7
+src=$HOME/Documents/$pfx
+pages="005 006"
 
-src=$HOME/Documents/Geant4OpticksWorkflow7
-snam=Geant4OpticksWorkflow7.005
-dnam=${snam/./_}
-ext=png
+for page in $pages ; do 
+    snam=${pfx}.${page}
+    dnam=${pfx}_${page}
+    ext=png
+    cmd="cp $src/$snam.$ext $dnam.$ext"
 
-cmd="cp $src/$snam.$ext $dnam.$ext"
-
-echo $cmd
-eval $cmd 
-
+    echo $cmd
+    eval $cmd 
+done
 
 
 
