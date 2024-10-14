e7e31b4bbc2eccf2c27c51e772cf1a75aa9272fa
Simon C Blyth <simoncblyth@gmail.com>
2024-10-14 22:38:12 +0800

    renders for presentation

---
+++ env/presentation/GEOM/scp_from_A.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# /Users/blyth/simoncblyth.bitbucket.io/env/presentation/GEOM
+mkdir -p J_2024aug27/CSGOptiXRenderTest/CVD0/80000/ALL
+scp A:/tmp/blyth/opticks/GEOM/J_2024aug27/CSGOptiXRenderTest/CVD0/80000/ALL/cxr_overview_emm_t0_elv_t_moi__ALL.{jpg,json,log,npy} J_2024aug27/CSGOptiXRenderTest/CVD0/80000/ALL/
+
