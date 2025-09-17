7f3ba36f05576d00e8af54c40885b047183012c7
Simon C Blyth <simoncblyth@gmail.com>
2025-09-17 16:40:18 +0800

    add logos for FastAPI, nanobind and Drogon

---
+++ env/presentation/FastAPI/get_logo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -L -O https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png
---
+++ env/presentation/nanobind/get_logo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -L -O https://raw.githubusercontent.com/wjakob/nanobind/master/docs/images/logo.jpg
+
